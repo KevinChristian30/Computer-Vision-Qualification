f65efdffe4810cc0f0f09762130a7cf9651135b2
Kevin Christian <kdotchrist30@gmail.com>
2023-05-03 21:40:08 +0700

    Finished Edge Detection

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Utility:
   def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-class EdgeDetection():
+class EdgeDetection:
+  def showResult(nrow = None, ncol = None, res_stack = None):
+    plt.figure(figsize=(12, 12))
+
+    for i, (label, image) in enumerate(res_stack):
+      plt.subplot(nrow, ncol, i + 1)
+      plt.imshow(image, cmap='gray')
+      plt.title(label)
+      plt.axis('off')
+  
+    plt.show()
+
   def start():
-    print('Edge Detection')
+    image = cv2.imread('./Edge Detection/rickroll.jpg')
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    laplace8u = cv2.Laplacian(gray_image, cv2.CV_8U)
+    laplace16s = cv2.Laplacian(gray_image, cv2.CV_16S)
+    laplace32f = cv2.Laplacian(gray_image, cv2.CV_32F)
+    laplace64f = cv2.Laplacian(gray_image, cv2.CV_64F)
+
+    laplace_labels = ['8U', '16S', '32F', '64F']
+    laplace_images = [laplace8u, laplace16s, laplace32f, laplace64f]
+
+    EdgeDetection.showResult(2, 2, zip(laplace_labels, laplace_images))
 
 class ShapeDetection:
   def start():
@@ -52,4 +77,3 @@ while (True):
   MenuFacade.displayMenu()
   option = input('>> ')
   MenuFacade.route(option)
-  input()
